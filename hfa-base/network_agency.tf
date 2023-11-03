617d766636ebeffaaeb8f036baed6c7f4f8f63a0
mu <agoodmu@gmail.com>
2023-11-03 09:39:30 +0800

    change network agency

--- hfa-base/network_agency.tf
+++ hfa-base/network_agency.tf
@@ -1,4 +1,4 @@
-resource "huaweicloud_identity_agency" "hfa_networkd_admin" {
+resource "huaweicloud_identity_agency" "hfa_network_admin" {
   name                  = var.hfa_network_admin_agency_name
   description           = "Manage all network resources arcoss HFA"
   delegated_domain_name = var.hfa_iam_account_name
