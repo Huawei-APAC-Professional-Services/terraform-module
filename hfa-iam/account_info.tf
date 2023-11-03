c4deca9fc149a1c3f5d91a6c90b8fe556788f5ac
mu <agoodmu@gmail.com>
2023-11-03 15:19:33 +0800

    update hfa-iam

--- hfa-iam/account_info.tf
+++ hfa-iam/account_info.tf
@@ -3,3 +3,7 @@ data "huaweicloud_account" "hfa_account" {}
 output "hfa_account_id" {
   value = data.huaweicloud_account.hfa_account.id
 }
+
+output "hfa_account_name" {
+  value = data.huaweicloud_account.hfa_account.name
+}
