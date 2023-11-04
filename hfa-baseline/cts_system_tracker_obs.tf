c7246eb0aea5e8e7b0e8e5e0a793f34a3b39c91f
mu <agoodmu@gmail.com>
2023-11-04 13:34:18 +0800

    add topic

--- hfa-baseline/cts_system_tracker_obs.tf
+++ hfa-baseline/cts_system_tracker_obs.tf
@@ -5,8 +5,10 @@ resource "huaweicloud_cts_tracker" "system_tracker" {
 }
 
 resource "huaweicloud_smn_topic" "hfa_cts_topic" {
-  name                     = "hfa_cts_topic"
-  display_name             = "hfa_cts_topic"
+  for_each = var.hfa_cts_regions
+  region = each.key
+  name                     = var.hfa_cts_smn_topic_name
+  display_name             = var.hfa_cts_smn_topic_display_name
   services_publish_allowed = "cts"
-  introduction             = "created by hfa"
+  introduction             = "created for hfa"
 }
--- hfa-baseline/variables.tf
+++ hfa-baseline/variables.tf
@@ -1,3 +1,13 @@
 variable "hfa_cts_regions" {
   type = map(string)
 }
+
+variable "hfa_cts_smn_topic_name" {
+  type = string
+  default = "hfa_cts_topic"
+}
+
+variable "hfa_cts_smn_topic_display_name" {
+  type = string
+  default = "hfa cts topic"
+}
