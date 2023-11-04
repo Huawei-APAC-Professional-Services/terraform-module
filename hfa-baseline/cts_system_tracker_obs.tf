6862eb617009787572374eb40bca2363bafebb2f
mu <agoodmu@gmail.com>
2023-11-04 13:53:27 +0800

    add notifications

--- hfa-baseline/cts_system_tracker_obs.tf
+++ hfa-baseline/cts_system_tracker_obs.tf
@@ -4,6 +4,15 @@ resource "huaweicloud_cts_tracker" "system_tracker" {
   bucket_name = each.value
 }
 
+data "huaweicloud_identity_group" "admin" {
+  name = "admin"
+}
+
+locals {
+  admin_users = toset([for u in data.huaweicloud_identity_group.admin.users: u.name])
+  smn_topics = { for v in huaweicloud_smn_topic.hfa_cts_topic : v.region => v.id }
+}
+
 resource "huaweicloud_smn_topic" "hfa_cts_topic" {
   for_each = var.hfa_cts_regions
   region = each.key
@@ -11,4 +20,23 @@ resource "huaweicloud_smn_topic" "hfa_cts_topic" {
   display_name             = var.hfa_cts_smn_topic_display_name
   services_publish_allowed = "cts"
   introduction             = "created for hfa"
+}
+
+resource "huaweicloud_cts_notification" "hfa_root_user_login" {
+  for_each = var.hfa_cts_regions
+  region = each.key
+  name           = "hfa_root_user_login"
+  operation_type = "customized"
+  smn_topic      = local.smn_topics[each.key]
+
+  operations {
+    service     = "IAM"
+    resource    = "user"
+    trace_names = var.hfa_root_user_monitored_activities
+  }
+  
+  operation_users {
+    group = "admin"
+    users = local.admin_users
+  }
 }
--- hfa-baseline/variables.tf
+++ hfa-baseline/variables.tf
@@ -11,3 +11,8 @@ variable "hfa_cts_smn_topic_display_name" {
   type = string
   default = "hfa cts topic"
 }
+
+variable "hfa_root_user_monitored_activities" {
+  type = list(string)
+  default = [ "login" ]
+}
