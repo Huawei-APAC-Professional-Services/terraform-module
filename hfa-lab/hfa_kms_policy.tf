87149dc782ef7efb00f9e3903ac3d09836616149
mu <agoodmu@gmail.com>
2023-11-07 12:15:28 +0800

    add policy documents

--- hfa-lab/hfa_kms_policy.tf
+++ hfa-lab/hfa_kms_policy.tf
@@ -9,5 +9,44 @@ resource "huaweicloud_identity_role" "kms_role" {
   name        = "hfa_terraform_kms"
   description = "Created for hfa workshop"
   type        = "AX"
-  policy      = jsonencode(file("kms_policy.json"))
+  policy = jsonencode({
+    "Version" : "1.1",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "kms:dek:encrypt",
+          "kms:cmk:getMaterial",
+          "kms:cmk:create",
+          "kms:grant:retire",
+          "kms:cmk:getRotation",
+          "kms:cmkTag:create",
+          "kms:cmk:decrypt",
+          "kms:partition:create",
+          "kms:cmk:update",
+          "kms:cmk:get",
+          "kms:dek:create",
+          "kms:partition:list",
+          "kms:partition:get",
+          "kms:grant:revoke",
+          "kms:cmk:encrypt",
+          "kms:cmk:getQuota",
+          "kms:cmk:list",
+          "kms:cmk:getInstance",
+          "kms:cmk:generate",
+          "kms:cmk:verify",
+          "kms:cmk:crypto",
+          "kms:cmk:sign",
+          "kms:dek:crypto",
+          "kms:dek:decrypt",
+          "kms:grant:create",
+          "kms:grant:list",
+          "kms:cmk:deleteMaterial",
+          "kms:cmk:getPublicKey",
+          "kms:cmkTag:list",
+          "kms:cmk:enable"
+        ]
+      }
+    ]
+  })
 }
