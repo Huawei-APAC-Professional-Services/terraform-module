014fbea5dfeef664b7a70579188520b04ba916f9
mu <agoodmu@gmail.com>
2023-11-08 15:07:23 +0800

    add new parameters

--- hfa-baseline/config_rules_rms.tf
+++ hfa-baseline/config_rules_rms.tf
@@ -9,7 +9,4 @@ resource "huaweicloud_rms_policy_assignment" "tracker_config_enabled_check" {
   policy_definition_id = try(data.huaweicloud_rms_policy_definitions.tracker_config_enabled_check.definitions[0].id, "")
   status               = "Enabled"
   period               = var.hfa_config_period
-  parameters = {
-    regionList = local.enalbed_regions
-  }
 }
