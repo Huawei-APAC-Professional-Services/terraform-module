43521d86863c289dd15e286e24d35657461feb97
mu <agoodmu@gmail.com>
2023-11-08 15:00:00 +0800

    add value

--- hfa-baseline/config_rules_cts.tf
+++ hfa-baseline/config_rules_cts.tf
@@ -37,9 +37,7 @@ resource "huaweicloud_rms_policy_assignment" "cts_kms_encrypted_check" {
 
 locals {
   // enalbed_regions = join("", ["[", join(",", [for k, v in var.hfa_cts_regions_obs_config : k]), "]"])
-  // enalbed_regions = tolist([for k, v in var.hfa_cts_regions_obs_config : k])
-  regions_list    = join(",", [for k, v in var.hfa_cts_regions_obs_config : k])
-  enalbed_regions = "\"[\"ap-southeast-1\",\"ap-southeast-2\",\"ap-southeast-3\"]\""
+  enalbed_regions = jsonencode(tolist([for k, v in var.hfa_cts_regions_obs_config : k]))
 }
 
 data "huaweicloud_rms_policy_definitions" "multi_region_cts_tracker_exists" {
