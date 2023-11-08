fe1c73414d6bf533126836aa6c3f50ed61e69804
mu <agoodmu@gmail.com>
2023-11-08 11:01:15 +0800

    add logical config

--- hfa-baseline/config.tf
+++ hfa-baseline/config.tf
@@ -2,10 +2,11 @@ data "huaweicloud_account" "current" {}
 
 locals {
   config_member_accounts = var.hfa_config_aggregator_account_id == data.huaweicloud_account.current.id ? [for account in var.hfa_accounts_id_list: account if account != data.huaweicloud_account.current.id ] : var.hfa_accounts_id_list
+  is_aggregator_account = var.hfa_config_aggregator_account_id == data.huaweicloud_account.current.id
 }
 
 resource "huaweicloud_rms_resource_aggregation_authorization" "main" {
-  count      = var.hfa_config_aggregator_account_id == data.huaweicloud_account.current.id ? 0 : 1
+  count      = local.is_aggregator_account ? 0 : 1
   account_id = var.hfa_config_aggregator_account_id
 }
 
@@ -31,7 +32,7 @@ resource "huaweicloud_rms_resource_recorder" "main" {
 }
 
 resource "huaweicloud_rms_resource_aggregator" "main" {
-  count       = var.hfa_config_aggregator_account_id == data.huaweicloud_account.current.id ? 1 : 0
+  count       = local.is_aggregator_account ? 1 : 0
   name        = var.hfa_config_aggregator_name
   type        = "ACCOUNT"
   account_ids = local.config_member_accounts
