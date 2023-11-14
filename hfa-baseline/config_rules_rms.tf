6fc88bfaaee79081afdb78e2ac0d3a889a0b683f
mu <agoodmu@gmail.com>
2023-11-14 15:12:37 +0800

    remove filter

--- hfa-baseline/config_rules_rms.tf
+++ hfa-baseline/config_rules_rms.tf
@@ -5,10 +5,6 @@ data "huaweicloud_rms_policy_definitions" "tracker_config_enabled_check" {
 resource "huaweicloud_rms_policy_assignment" "tracker_config_enabled_check" {
   name                 = "tracker-config-enabled-check"
   policy_definition_id = try(data.huaweicloud_rms_policy_definitions.tracker_config_enabled_check.definitions[0].id, "")
-  policy_filter {
-    resource_provider = "config"
-    resource_type     = "trackers"
-  }
   status = "Enabled"
   period = var.hfa_config_period
 }
