843dc35eafa05525dfdc8c7e6503820602322e41
mu <agoodmu@gmail.com>
2024-03-15 13:01:26 +0800

    add org accounts

---
+++ hfa-management/main.tf
@@ -0,0 +1,6 @@
+resource "huaweicloud_organizations_account" "huawei_cloud_account"{
+  name  = var.account_name
+  email = var.email_address
+  agency_name = var.organization_agency_name
+  parent_id = var.parent_id
+}
