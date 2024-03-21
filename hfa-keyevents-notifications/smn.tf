0b5bb44cdb5d2019fbbafd4b3b2de2b74a4b9bea
mu <agoodmu@gmail.com>
2024-03-21 18:00:16 +0800

    add all events

--- hfa-keyevents-notifications/smn.tf
+++ hfa-keyevents-notifications/smn.tf
@@ -5,14 +5,14 @@ resource "huaweicloud_smn_topic" "main" {
 }
 
 resource "huaweicloud_smn_subscription" "email_notification" {
-  for_each  = var.email_recipients_list
+  for_each  = toset(var.email_recipients_list)
   topic_urn = huaweicloud_smn_topic.main.id
   endpoint  = each.value
   protocol  = "email"
 }
 
 resource "huaweicloud_smn_subscription" "sms_notification" {
-  for_each  = var.sms_recipients_list
+  for_each  = toset(var.sms_recipients_list)
   topic_urn = huaweicloud_smn_topic.main.id
   endpoint  = each.value
   protocol  = "email"
