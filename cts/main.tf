5e71462a5798216470364ff24a993b757f6fbc50
Jimmy Mujianmin <jimmy.mujianmin@globe.com.ph>
2024-11-07 17:36:46 +0800

    Add CTS

---
+++ cts/main.tf
@@ -0,0 +1,23 @@
+resource "huaweicloud_obs_bucket" "this" {
+  bucket        = var.cts_bucket_name
+  acl           = "private"
+  storage_class = var.cts_bucket_storage_class
+  versioning    = var.cts_bucket_enable_versioning
+  lifecycle_rule {
+    name    = "retention"
+    enabled = true
+
+    expiration {
+      days = var.cts_bucket_log_retention_period
+    }
+    abort_incomplete_multipart_upload {
+      days = 30
+    }
+  }
+  multi_az      = true
+  encryption    = true
+  sse_algorithm = var.cts_bucket_sse_algorithm
+  kms_key_id    = var.cts_encryption_kms_id
+
+  tags = var.cts_bucket_tags
+}
---
+++ cts/variables.tf
@@ -0,0 +1,33 @@
+variable "cts_bucket_name" {
+  type = string
+}
+
+variable "cts_bucket_tags" {
+  type    = map(string)
+  default = {}
+}
+
+variable "cts_bucket_storage_class" {
+  type    = string
+  default = "STANDARD"
+}
+
+variable "cts_bucket_enable_versioning" {
+  type    = bool
+  default = true
+}
+
+variable "cts_bucket_log_retention_period" {
+  type    = number
+  default = 3650
+}
+
+variable "cts_bucket_sse_algorithm" {
+  type    = string
+  default = "AES256"
+}
+
+variable "cts_encryption_kms_id" {
+  type    = string
+  default = null
+}
