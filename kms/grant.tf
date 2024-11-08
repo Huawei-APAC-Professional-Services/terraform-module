4c6a52fbb6476bf159cd0763dec01ba3b8aab0b1
Jimmy Mujianmin <jimmy.mujianmin@globe.com.ph>
2024-11-09 00:36:22 +0800

    Update KMS

--- kms/grant.tf
+++ kms/grant.tf
@@ -1,5 +1,5 @@
 resource "huaweicloud_kms_grant" "this" {
-  for_each           = var.grantees
+  for_each           = toset(var.grantees)
   key_id             = huaweicloud_kms_key.this.key_id
   type               = each.value["type"]
   grantee_principal  = each.value["grantee_principal"]
