bb6e06c9712555ba9a4a15c77ffb28dd53d26f7b
agoodmu <135948737+agoodmu@users.noreply.github.com>
2024-11-10 08:11:29 +0000

    update

--- logstash/cluster.tf
+++ logstash/cluster.tf
@@ -1,18 +1,34 @@
 resource "huaweicloud_css_logstash_cluster" "this" {
   name           = var.name
-  engine_version = "7.10.0"
-
-  node_config {
-    flavor          = "ess.spec-4u8g"
-    instance_number = 1
-    volume {
-      volume_type = "HIGH"
-      size        = 40
-    }
-  }
+  engine_version = var.engine_version
+  charging_mode = var.charging_mode
+  period_unit = var.period_unit
+  period = var.period
 
   availability_zone = var.availability_zone
   vpc_id            = var.vpc_id
   subnet_id         = var.subnet_id
   security_group_id = var.secgroup_id
+
+  dynamic node_config {
+    for_each = var.node_config
+    content {
+        flavor          = node_config.value["flavor"]
+        instance_number = node_config.value["instance_number"]
+        volume {
+            volume_type = node_config.value["volume_type"]
+            size        = node_config.value["volume_size"]
+        }
+    }
+  }
+
+  dynamic routes {
+    for_each = var.routes
+    content {
+        ip_address = routes.value["ip_address"]
+        ip_net_mask = routes.value["ip_net_mask"]
+    }
+  }
+
+  tags = var.tags
 }
--- logstash/variables.tf
+++ logstash/variables.tf
@@ -6,3 +6,57 @@ variable "engine_version" {
     type = string
     default = "7.10.0
 }
+
+variable "availability_zone" {
+    type = string
+}
+
+variable "vpc_id" {
+    type = string
+}
+
+variable "subnet_id" {
+    type = string
+}
+
+variable "secgroup_id" {
+    type = string
+}
+
+variable "tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "charging_mode" {
+    type = string
+    default = "postPaid"
+}
+
+variable "period_unit" {
+    type = string
+    default = null
+}
+
+variable "period" {
+    type = number
+    default = null
+}
+
+variable "node_config" {
+    type = list(object({
+        flavor = string
+        instance_number = number
+        volume_type = optional(string, "ULTRAHIGH")
+        volume_size = number
+    }))
+    default = []
+}
+
+variable "routes" {
+    type = list(object({
+        ip_address = string
+        ip_net_mask = string
+    }))
+    default = []
+}
