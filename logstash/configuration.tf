e2aa32fc3c2ec9dc79a6d20f64b6efc057e007ff
Jimmy Mujianmin <jimmy.mujianmin@globe.com.ph>
2024-11-11 13:07:39 +0800

    Update logstash configuration

--- logstash/configuration.tf
+++ logstash/configuration.tf
@@ -1,15 +1,15 @@
 resource "huaweicloud_css_logstash_configuration" "this" {
-  for_each     = var.logstash_config
+  for_each     = { for config in var.logstash_config : config.name => config }
   cluster_id   = huaweicloud_css_logstash_cluster.this.id
-  name         = each.value["name"]
-  conf_content = each.value["conf_content"]
+  name         = each.value.name
+  conf_content = each.value.conf_content
 
   setting {
-    workers                  = each.value["workers"]
-    batch_size               = each.value["batch_size"]
-    batch_delay_ms           = each.value["batch_delay_ms"]
-    queue_type               = each.value["queue_type"]
-    queue_check_point_writes = each.value["queue_check_point_writes"]
-    queue_max_bytes_mb       = each.value["queue_max_bytes_mb"]
+    workers                  = each.value.workers
+    batch_size               = each.value.batch_size
+    batch_delay_ms           = each.value.batch_delay_ms
+    queue_type               = each.value.queue_type
+    queue_check_point_writes = each.value.queue_check_point_writes
+    queue_max_bytes_mb       = each.value.queue_max_bytes_mb
   }
 }
