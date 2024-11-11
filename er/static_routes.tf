d6efbe69d738fe1ab119a4275aa9f6a630c72d0a
Jimmy Mujianmin <jimmy.mujianmin@globe.com.ph>
2024-11-11 21:40:44 +0800

    update ER

--- er/static_routes.tf
+++ er/static_routes.tf
@@ -1,5 +1,5 @@
 resource "huaweicloud_er_static_route" "this" {
-  for_each       = { for route in var.var.static_routes : route.name => route }
+  for_each       = { for route in var.static_routes : route.name => route }
   route_table_id = huaweicloud_er_route_table.this[each.value.route_table_name].id
   destination    = each.value.destination_cidr
   attachment_id  = each.value.attachment_name != null ? huaweicloud_er_vpc_attachment.this[each.value.attachment_name].id : null
