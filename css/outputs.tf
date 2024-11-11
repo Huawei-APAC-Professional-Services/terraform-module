a2cd807a3a03e61724e64737576258593479892e
Jimmy Mujianmin <jimmy.mujianmin@globe.com.ph>
2024-11-11 12:34:26 +0800

    Update CSS Output

--- css/outputs.tf
+++ css/outputs.tf
@@ -1,7 +1,15 @@
 output "data_nodes" {
-  value = [for node in huaweicloud_css_cluster.this.nodes : node.ip if node.type == "ess-indicates"]
+  value = [for node in huaweicloud_css_cluster.this.nodes : node.ip if node.type == "ess"]
 }
 
-output "test" {
-  value = huaweicloud_css_cluster.this.nodes
+output "ess_master_nodes" {
+  value = [for node in huaweicloud_css_cluster.this.nodes : node.ip if node.type == "ess-master"]
+}
+
+output "ess_client_nodes" {
+  value = [for node in huaweicloud_css_cluster.this.nodes : node.ip if node.type == "ess-client"]
+}
+
+output "ess_cold_nodes" {
+  value = [for node in huaweicloud_css_cluster.this.nodes : node.ip if node.type == "ess-cold"]
 }
