32fc2623040daf40be6f655060f7b59ea355debf
Jimmy Mujianmin <jimmy.mujianmin@globe.com.ph>
2024-11-11 12:57:36 +0800

    Update CSS Output

--- css/outputs.tf
+++ css/outputs.tf
@@ -1,15 +1,15 @@
 output "data_nodes" {
-  value = [for node in huaweicloud_css_cluster.this.nodes : node.ip if node.type == "ess"]
+  value = [for index, node in huaweicloud_css_cluster.this.nodes : node.ip if node.type == "ess"]
 }
 
 output "ess_master_nodes" {
-  value = [for node in huaweicloud_css_cluster.this.nodes : node.ip if node.type == "ess-master"]
+  value = [for index, node in huaweicloud_css_cluster.this.nodes : node.ip if node.type == "ess-master"]
 }
 
 output "ess_client_nodes" {
-  value = [for node in huaweicloud_css_cluster.this.nodes : node.ip if node.type == "ess-client"]
+  value = [for index, node in huaweicloud_css_cluster.this.nodes : node.ip if node.type == "ess-client"]
 }
 
 output "ess_cold_nodes" {
-  value = [for node in huaweicloud_css_cluster.this.nodes : node.ip if node.type == "ess-cold"]
+  value = [for index, node in huaweicloud_css_cluster.this.nodes : node.ip if node.type == "ess-cold"]
 }
