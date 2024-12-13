93270b3d6d4c19fd84cb62424d3fff2395622789
Jimmy Mujianmin <jimmy.mujianmin@globe.com.ph>
2024-12-13 10:24:39 +0800

    Delete some module

--- router53/hwdns.tf
+++
@@ -1,5 +0,0 @@
-resource "huaweicloud_dns_zone" "main" {
-  name      = aws_route53_record.main.name
-  ttl       = 3000
-  zone_type = "public"
-}
--- router53/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "huawednsnameservers" {
-  type    = list(string)
-  default = ["ns1.huaweicloud-dns.com.", "ns1.huaweicloud-dns.cn.", "ns1.huaweicloud-dns.net.", "ns1.huaweicloud-dns.org."]
-}
-
-variable "router53zone" {
-  type = string
-}
