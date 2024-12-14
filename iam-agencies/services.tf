ff5f6e017a2d1af9af79269dd3f1bbb5ff79242b
Jimmy Mujianmin <jimmy.mujianmin@globe.com.ph>
2024-12-14 16:16:32 +0800

    update

--- iam-agencies/services.tf
+++ iam-agencies/services.tf
@@ -1,16 +1,16 @@
 resource "huaweicloud_identity_agency" "this" {
-  for_each              = var.agencies
-  name                  = each.value.name
-  description           = each.value.description
-  delegated_domain_name = each.value.delegated_domain_name
+  for_each              = { for agency in var.var.agencies : agency.name => agency }
+  name                  = each.value["name"]
+  description           = each.value["description"]
+  delegated_domain_name = each.value["delegated_domain_name"]
 
   dynamic "project_role" {
-    for_each = each.value.project_role
+    for_each = each.value["project_roles"]
     content {
       project = project_role.key
       roles   = project_role.value
     }
   }
-  domain_roles        = each.value.domain_roles
-  all_resources_roles = each.value.all_resources_roles
+  domain_roles        = each.value["domain_roles"]
+  all_resources_roles = each.value["all_resources_roles"]
 }
--- iam-agencies/variables.tf
+++ iam-agencies/variables.tf
@@ -4,7 +4,7 @@ variable "agencies" {
     description           = optional(string, null)
     delegated_domain_name = string
     duration              = optional(string, "FOREVER")
-    project_role          = optional(map(string), {})
+    project_roles         = optional(map(string), {})
     domain_roles          = optional(list(string), null)
     all_resources_roles   = optional(list(string), null)
   }))
