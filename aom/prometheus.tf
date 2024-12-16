2696b8486b62b3ffc1bcee0ea20390394317d7cd
Jimmy Mujianmin <jimmy.mujianmin@globe.com.ph>
2024-12-16 12:10:55 +0800

    add aom module

--- aom/prometheus.tf
+++ aom/prometheus.tf
@@ -1,5 +1,5 @@
 resource "huaweicloud_aom_prom_instance" "this" {
-  for_each              = { for instance in var.var.prometheus_instances : instance.name => instance }
+  for_each              = { for instance in var.prometheus_instances : instance.name => instance }
   prom_name             = each.value["name"]
   prom_type             = each.value["type"]
   enterprise_project_id = each.value["enterprise_project_id"]
