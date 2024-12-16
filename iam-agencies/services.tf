a360858244715895f5f53f3a38109de64fac3db2
Jimmy Mujianmin <jimmy.mujianmin@globe.com.ph>
2024-12-16 12:53:27 +0800

    update agency module

--- iam-agencies/services.tf
+++ iam-agencies/services.tf
@@ -1,5 +1,13 @@
+resource "huaweicloud_identity_service_agency" "this" {
+  for_each               = { for agency in var.service_agencies : agency.name => agency }
+  name                   = each.value["name"]
+  description            = each.value["description"]
+  delegated_service_name = each.value["delegated_service_name"]
+  policy_names           = each.value["policy_names"]
+}
+
 resource "huaweicloud_identity_agency" "this" {
-  for_each              = { for agency in var.agencies : agency.name => agency }
+  for_each              = { for agency in var.account_agencies : agency.name => agency }
   name                  = each.value["name"]
   description           = each.value["description"]
   delegated_domain_name = each.value["delegated_domain_name"]
--- iam-agencies/variables.tf
+++ iam-agencies/variables.tf
@@ -1,4 +1,14 @@
-variable "agencies" {
+variable "service_agencies" {
+  type = list(object({
+    name                   = string
+    description            = optional(string, null)
+    delegated_service_name = string
+    duration               = optional(string, "FOREVER")
+    policy_names           = optional(list(string), null)
+  }))
+}
+
+variable "account_agencies" {
   type = list(object({
     name                  = string
     description           = optional(string, null)
