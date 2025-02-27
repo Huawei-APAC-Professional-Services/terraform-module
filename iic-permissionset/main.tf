8b320fe667085ea799b73b1fe765296800037c6d
Jimmy Mujianmin <jimmy.mujianmin@globe.com.ph>
2025-02-27 10:51:27 +0800

    update

--- iic-permissionset/main.tf
+++ iic-permissionset/main.tf
@@ -1,5 +1,5 @@
 resource "huaweicloud_identitycenter_permission_set" "this" {
-  for_each         = toset(var.permission_sets)
+  for_each         = { for permissionset in var.permission_sets : permissionset.name => permissionset }
   instance_id      = data.huaweicloud_identitycenter_instance.this.id
   name             = each.value.name
   session_duration = each.value.session_duration
