cb6e38269cc1b94222828a6b5909aa2025e212a7
Jimmy Mujianmin <jimmy.mujianmin@globe.com.ph>
2025-03-03 15:21:45 +0800

    update cce

--- cce/output.tf
+++ cce/output.tf
@@ -1,19 +1,19 @@
 output "cluster_api_server_private" {
-  value = [for s in huaweicloud_cce_cluster.this.certificate_clusters : s.server if s.name == "internalCluster"][0]
+  value = [for c in huaweicloud_cce_cluster.this.certificate_clusters : c.server if c.name == "internalCluster"][0]
 }
 
 output "cluster_api_server_public" {
-  value = [for s in huaweicloud_cce_cluster.this.certificate_clusters : s.server if s.name == "externalCluster"][0]
+  value = [for c in huaweicloud_cce_cluster.this.certificate_clusters : c.server if c.name == "externalCluster"][0]
 }
 
 output "cluster_ca_certificate" {
-  value = [for s in huaweicloud_cce_cluster.this.certificate_clusters : s.certificate_authority_data if s.name == "externalClusterTLSVerify"][0]
+  value = [for c in huaweicloud_cce_cluster.this.certificate_clusters : c.certificate_authority_data if c.name == "externalClusterTLSVerify"][0]
 }
 
 output "cluster_client_key" {
-  value = [for u in huaweicloud_cce_cluster.this.certificate_users : s.client_certificate_data if s.name == "user"][0]
+  value = [for u in huaweicloud_cce_cluster.this.certificate_users : u.client_certificate_data if u.name == "user"][0]
 }
 
 output "cluster_client_certificate" {
-  value = [for u in huaweicloud_cce_cluster.this.certificate_users : s.client_key_data if s.name == "user"][0]
+  value = [for u in huaweicloud_cce_cluster.this.certificate_users : u.client_key_data if u.name == "user"][0]
 }
