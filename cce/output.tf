578eeb47dcaf01c78148282a7b297d2e08b217ce
Jimmy Mujianmin <jimmy.mujianmin@globe.com.ph>
2025-03-03 14:18:25 +0800

    update cce

---
+++ cce/output.tf
@@ -0,0 +1,19 @@
+output "cluster_api_server_private" {
+  value = [for s in huaweicloud_cce_cluster.this.certificate_clusters : s.server if s.name == "internalCluster"][0]
+}
+
+output "cluster_api_server_public" {
+  value = [for s in huaweicloud_cce_cluster.this.certificate_clusters : s.server if s.name == "externalCluster"][0]
+}
+
+output "cluster_ca_certificate" {
+  value = [for s in huaweicloud_cce_cluster.this.certificate_clusters : s.certificate_authority_data if s.name == "externalClusterTLSVerify"][0]
+}
+
+output "cluster_client_key" {
+  value = huaweicloud_cce_cluster.this.certificate_users
+}
+
+output "cluster_client_certificate" {
+  value = huaweicloud_cce_cluster.this.certificate_users
+}
